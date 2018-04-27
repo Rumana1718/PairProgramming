646a8c1457d02b60b0fb5a6e1774340f57a8f670
Rumana Islam <37121430+Rumana1718@users.noreply.github.com>
2018-04-27 23:16:48 +0600

    Update doctor.cpp

--- doctor.cpp
+++ doctor.cpp
@@ -15,5 +15,32 @@ int main()
         <<"4.I don't need anything"<<endl;
     cin>>n;
     
-   
+   if(n==1)
+    {
+        currentMedication();
+
+
+    }
+       
+    else if(n==2)
+       
+    {
+       
+        newMedication();
+       
+    }
+       
+    else if(n==3)
+       
+    {
+       
+        formulary();
+       
+    }
+       
+    }
+    while(n!=4);
+
+
+    return 0;
 }
